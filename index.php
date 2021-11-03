79598128ee75a222803b809f089b38c6f2e6fa75
germantorres1995 <93538555+germantorres1995@users.noreply.github.com>
2021-11-03 11:07:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
   $ang1 = $_POST['ang1'];
   $ang2 = $_POST['ang2'];
+  $ang3 = $_POST['ang3'];
+  $ang4 = $_POST['ang4'];
+  $ang5 = $_POST['ang5'];
+  $ang6 = $_POST['ang6'];
 
-  $info = "[" . date("Y m d H:i:s") . "]". ";" . $ang1 . ";" . $ang2 . ";" . "\n";  
+  $info = "[" . date("Y m d H:i:s") . "]". ";" . $ang1 . ";" . $ang2 . ";" . $ang3 . ";" . $ang3 . ";" . $ang4 . ";" . $ang5 . ";" . $ang6 . ";" . "\n";  
   $nombre_archivo = "status.info";
   if($archivo = fopen($nombre_archivo, "w"))
   {
@@ -16,6 +20,8 @@
     fwrite($archivo, $info);
     fclose($archivo);
   }
-  header('Location: /TI-ISC-Laboratorio4/paginaPrincipal.html');
+  header('Location: /Paginas/act4.html');
+  //header('Location: /TI-ISC-Laboratorio4/paginaPrincipal.html');
+
 
 ?>
