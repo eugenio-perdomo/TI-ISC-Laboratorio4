59f42448f6e5c87f80c0ca269cbedc80a7dc9d5a
germantorres1995 <93538555+germantorres1995@users.noreply.github.com>
2021-11-06 13:36:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+  date_default_timezone_set('America/Argentina/Salta');
+
   $ang1 = $_POST['ang1'];
   $ang2 = $_POST['ang2'];
   $ang3 = $_POST['ang3'];
@@ -6,7 +9,7 @@
   $ang5 = $_POST['ang5'];
   $ang6 = $_POST['ang6'];
 
-  $info = "[" . date("Y m d H:i:s") . "]". ";" . $ang1 . ";" . $ang2 . ";" . $ang3 . ";" . $ang3 . ";" . $ang4 . ";" . $ang5 . ";" . $ang6 . ";" . "\n";  
+  $info = "[" . date("Y-m-d H:i:s") . "]". ";" . $ang1 . ";" . $ang2 . ";" . $ang3 . ";" . $ang3 . ";" . $ang4 . ";" . $ang5 . ";" . $ang6 . ";" . "\n";  
   $nombre_archivo = "status.info";
   if($archivo = fopen($nombre_archivo, "w"))
   {
@@ -20,8 +23,8 @@
     fwrite($archivo, $info);
     fclose($archivo);
   }
-  header('Location: /Paginas/act4.html');
-  //header('Location: /TI-ISC-Laboratorio4/paginaPrincipal.html');
+
+  header('Location: /TI-ISC-Laboratorio4/paginaPrincipal.html');
 
 
 ?>
